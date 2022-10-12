40c4aedd3be3246045c564702d9ec68ca84f851b
xs248 <xs248@cornell.edu>
2022-10-12 18:45:47 -0400

    new

--- static/js/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-// Project imports
-import "./index.css";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
