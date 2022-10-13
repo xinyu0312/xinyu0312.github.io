cb1ad69bb086f2a6e1b7e3edcf5a9f0a779590e6
xs248 <xs248@cornell.edu>
2022-10-12 21:25:21 -0400

    d

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -9,6 +9,7 @@ function Contact() {
     <label className="section-title">Contact</label>
     <div className="contact-container">
       <div className="contact-left">
+        
         <p>Want to get in touch? Contact me on any of the platform</p>
         <SocialContact />
         </div>
