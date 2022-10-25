6f4210316d8b6c86c0cbdb0696f8800e3295d1fd
xs248 <xs248@cornell.edu>
2022-10-25 14:59:45 -0400

    x

--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -6,32 +6,37 @@ function Web() {
 
     <div className="web-option">
         <a href="#top">
-        <i class="fi-rr-home option-icon"></i>Home
+        {/* <i class="fi-rr-home option-icon"></i>Home */}
+        <i class="fa-solid fa-house option-icon"></i>Home
         </a>
     </div>
 
     <div className="web-option">
         <a href="#about">
-        <i class="fi-rr-user option-icon"></i>About
+        <i class="fa-solid fa-user option-icon"></i>About
+        {/* <i class="fi-rr-user option-icon"></i>About */}
         </a>
     </div>
 
     <div className="web-option">
         <a href="#work">
-        <i class="fi-rr-briefcase option-icon"></i>Work
+        {/* <i class="fi-rr-briefcase option-icon"></i>Work */}
+        <i class="fa-solid fa-briefcase option-icon"></i>Work
         </a>
     </div>
 
     <div className="web-option">
         <a href="#projects" >
-          <i class="fi-rr-edit-alt option-icon"></i>Projects
+        <i class="fa-solid fa-pen-to-square option-icon"></i>Projects
+          {/* <i class="fi-rr-edit-alt option-icon"></i>Projects */}
         </a>
     </div>
 
  
     <div className="web-option">
         <a href="#contact">
-        <i class="fi-rr-envelope option-icon"></i>Contact
+        <i class="fa-solid fa-envelope option-icon"></i>Contact
+        {/* <i class="fi-rr-envelope option-icon"></i>Contact */}
         </a>
     </div>
 
